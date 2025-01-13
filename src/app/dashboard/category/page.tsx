a297b32ec7e93163d5e1e99b99ca7242f3482268
Ítalo Monte <italopessoal@outlook.com>
2025-01-13 17:03:51 -0400

    feat: category form and register

---
+++ src/app/dashboard/category/page.tsx
@@ -0,0 +1,56 @@
+import styles from "./styles.module.scss"
+import { Button } from "../components/button"
+import { api } from "@/services/app"
+import {getCookiesServer} from "@/lib/cookieServer"
+import { redirect } from "next/navigation"
+
+
+export default async function Category(){
+
+    async function handleRegisterCategory(formData: FormData) {
+        "use server" // quero que essa função seja executada no servidor
+
+        const name = formData.get("name") // propriedade name do input
+
+        if (name == "" ) return;
+
+        const data = {
+            name
+        }
+
+        const token = await getCookiesServer()
+
+        await api.post("/category", data, {  // fazendo requisição de post na rota /category
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+
+        redirect("/dashboard")
+        
+    }
+
+    return(
+        <main className={styles.container}>
+            <h1>Nova Category</h1>
+
+            <form 
+            action= {handleRegisterCategory}
+            className={styles.form}>
+                <input 
+                type="text" 
+                name="name"
+                placeholder="Nome da categoria, ex: Pizzas"
+                className={styles.input}
+                />
+
+                <Button
+                    text="Cadastrar"
+                />
+            </form>
+        </main>
+    )
+}
---
+++ src/app/dashboard/components/button/index.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import styles  from "./styles.module.scss"
+import { useFormStatus } from "react-dom"
+
+interface Props {
+    text: string
+}
+
+export function Button( {text}: Props) {
+
+    const {pending} = useFormStatus()
+
+    return(
+        <button type="submit" disabled={pending} className={styles.button}>
+            
+            {pending ? "Carregando..." : text}
+            
+        </button>
+    )
+}
