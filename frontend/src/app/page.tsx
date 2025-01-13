11d3429d899de94ac0aebbe801ce6b2ab21182dd
Ítalo Monte <italopessoal@outlook.com>
2025-01-12 20:25:08 -0400

    feat: store tokein in cookies

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { api } from '@/services/app'
 import { redirect } from 'next/navigation'
+import { cookies } from 'next/headers'
 
 export default function Home() {
 
@@ -30,7 +31,15 @@ export default function Home() {
 
       console.log(response)
 
+      const expressTime = 60 * 60 * 24 * 1000 // 24hrs
+      const cookieStore = await cookies()
 
+      cookieStore.set("session", response.data.token, {
+        maxAge: expressTime,
+        path: "/",
+        httpOnly: false,
+        secure: process.env.NODE_ENV === "production"
+      })
     } catch (error) {
       console.log(error)
       return
