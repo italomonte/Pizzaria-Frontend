8543c047aa0215146fdd3c60082c37b386e3b335
Ítalo Monte <italopessoal@outlook.com>
2025-01-12 23:06:06 -0400

    feat: middleware and validating token

---
+++ src/middleware.ts
@@ -0,0 +1,46 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { getCookiesServer } from './lib/cookieServer'
+import { headers } from 'next/headers'
+import { api } from './services/app'
+
+export async function middleware(req: NextRequest) {
+
+    const { pathname } = req.nextUrl
+
+    if (pathname.startsWith("/_next") || pathname === "/")  {
+        console.log("passou no middleware")
+
+        return NextResponse.next()
+    } else {
+        const token = await getCookiesServer()
+        
+        if (!token) {
+            return NextResponse.redirect(new URL("/", req.url))
+        }
+        
+        const isValid = await validateToken(token)
+
+        if (!isValid) {
+            return NextResponse.redirect(new URL("/", req.url))
+
+        } 
+    }
+}
+
+async function validateToken(token:string) {
+    if (!token) return false
+
+    try {
+        await api.get("/me", {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+
+        return true
+    } catch (error) {
+        console.log(error)
+        return false
+    }
+    
+}
